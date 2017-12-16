6a61803f85bbc0e114df4e8dd1941098bdbc36e2
Daniel Wnuk <dan@o2.pl>
2017-12-16 11:21:51 +0100

    ciąg dalszy

--- zajecia/javascript/js/variables.js
+++ zajecia/javascript/js/variables.js
@@ -8,3 +8,54 @@ imie = imieDamskie;
 
 console.log(imie);
 console.log(wiek);
+
+function wyswietlZmienna() {
+//    tutaj kod funkcji
+    
+    var nazwisko = 'kowalski';
+    console.log(nazwisko);
+}
+//wywołanie funkcji
+wyswietlZmienna();
+
+var zmienna = function () {
+    console.log('funkcja anonimowa');
+}
+
+zmienna();
+
+function mnozenie (parametr1, parametr2, parametr3) {
+    var wynik = parametr1 * parametr2 * parametr3;
+    return wynik;
+}
+
+mnozenie(3, 4, 5);
+
+var liczba1 = 5;
+var liczba2 = 10;
+var liczba3 = 3;
+
+console.log(mnozenie(liczba1, liczba2, liczba3));
+
+var wynikDzialaniaFunkcji = mnozenie(liczba1, 34, liczba2);
+console.log(wynikDzialaniaFunkcji);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
