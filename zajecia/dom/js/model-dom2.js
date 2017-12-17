07fffd7f6ccb026851f89c49faa1fbe998ee7593
Daniel Wnuk <dan@o2.pl>
2017-12-17 10:45:55 +0100

    nodes

--- zajecia/dom/js/model-dom2.js
+++ zajecia/dom/js/model-dom2.js
@@ -1,4 +1,12 @@
 var pierwszyDiv = document.getElementById('parFirst');
 console.log(pierwszyDiv);
-console.log(pierwszyDiv.parentNode);
-console.log(pierwszyDiv.parentElement);
+//console.log(pierwszyDiv.parentNode);
+//console.log(pierwszyDiv.parentElement);
+//
+//console.log(document.documentElement.parentNode);  /* document node */
+//console.log(document.documentElement.parentElement); /* null */
+
+console.log(pierwszyDiv.childNodes);
+console.log(pierwszyDiv.children);
+
+console.log(pierwszyDiv.firstChild); 
