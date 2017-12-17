a91cdf6185a289f0174eb27ff98ef5732ab34ae1
Daniel Wnuk <dan@o2.pl>
2017-12-17 14:06:37 +0100

    events

--- zajecia/dom/js/event.js
+++ zajecia/dom/js/event.js
@@ -1,2 +1,34 @@
 'use strict'
 
+function alarm(event) {
+    event.preventDefault(); //zapobiegamy domyślnej akcji
+    console.log('kliknięto w link');
+    console.log(event.type);
+}
+
+var secondLink = document.getElementsByTagName('a')[1];
+
+secondLink.onclick = alarm;
+
+var thirdLink = document.getElementsByTagName('a')[2];
+
+
+thirdLink.removeEventListener('click', alarm);
+
+
+thirdLink.addEventListener('click', function(){
+    alert('funkcja anonimowa');
+});
+
+function klikHeader() {
+    console.log('Kliknąłeś header');
+}
+
+document.getElementsByTagName('header')[0].onclick = klikHeader;
+
+function klikh1(event) {
+    event.stopPropagation();
+    console.log('hahahaha');
+}
+
+document.getElementsByTagName('h1')[0].onclick = klikh1;
