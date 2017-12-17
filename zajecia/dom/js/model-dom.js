8e290bb62a3ba62435b60ac2cdd1788d57f2d3a9
Daniel Wnuk <dan@o2.pl>
2017-12-17 10:30:39 +0100

    JS dom

---
+++ zajecia/dom/js/model-dom.js
@@ -0,0 +1,22 @@
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var linki = document.getElementsByClassName('superlink');
+console.log(linki);
+
+var linkiPoTagu = document.getElementsByTagName('a');
+console.log(linkiPoTagu);
+
+ var divPoId = document.querySelector('#parSecond');
+console.log(divPoId);
+
+var linkiPoSelektorze = document.querySelectorAll('.link');
+console.log(linkiPoSelektorze);
+
+linkiPoSelektorze.forEach(function(link){
+    console.log(link)
+});
+
+for(var i = 0; i < linkiPoSelektorze.lenght; i++) {
+    console.log(linkiPoSelektorze[i]);
+}
---
+++ zajecia/dom/js/model-dom2.js
@@ -0,0 +1,4 @@
+var pierwszyDiv = document.getElementById('parFirst');
+console.log(pierwszyDiv);
+console.log(pierwszyDiv.parentNode);
+console.log(pierwszyDiv.parentElement);
